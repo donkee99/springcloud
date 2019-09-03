3e76acdebab53a3e12352fb33a96abb2a8eb16c7
donkee99 <donkee@163.com>
2019-09-03 16:50:31 +0800

    eureka-hystrix-dashboard-turbine

---
+++ eureka-hystrix-dashboard-turbine/eureka-feign-client/src/main/java/com/donkee/client/config/DashboardConfig.java
@@ -0,0 +1,23 @@
+package com.donkee.client.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Donkee
+ * @since 2019/9/3 11:02
+ */
+@Configuration
+public class DashboardConfig {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet =new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean =new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
